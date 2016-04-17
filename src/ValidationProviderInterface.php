25147edbec014c8fb90c8cc6379e130d92774a25
abdul <abdul@easyfundraising.org.uk>
2016-04-17 15:50:09 +0100

    Initial interface

---
+++ src/ValidationProviderInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TheSupportGroup\Common\ValidationInterop;
+
+/**
+ * Provides an interface for validation provider.
+ */
+interface ValidationProviderInterface
+{
+    public function rule();
+}
